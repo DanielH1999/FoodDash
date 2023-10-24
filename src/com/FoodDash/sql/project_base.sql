988037393be1acedc9c08f88ff8218cfac9a4c81
DaHero2031 <dagadan23@hotmail.com>
2023-10-24 19:23:28 -0300

    Add files via upload

--- src/com/FoodDash/sql/project_base.sql
+++ src/com/FoodDash/sql/project_base.sql
@@ -10,6 +10,7 @@ telefono varchar(10) not null
 create table if not exists restaurant(
 id_restaurant int auto_increment primary key,
 id_pedido int,
+id_envio int,
 nombre varchar(20) not null,
 telefono int
 );
@@ -31,8 +32,6 @@ ALTER TABLE restaurant ADD CONSTRAINT FOREIGN KEY (id_pedido) REFERENCES pedido(
 
 create table if not exists envio(
 id_envio int auto_increment primary key,
-id_cliente int not null,
-id_pedido int not null,
 id_restaurant int not null,
 direccion varchar(40) not null,
 medio_pago varchar(10) not null,
@@ -40,9 +39,6 @@ precio_envio float not null,
 estado varchar(10)
 );
 
--- alter table envio add constraint foreign key (id_cliente) references cliente(id_cliente) on delete cascade;
--- alter table envio add constraint foreign key (id_pedido) references pedido(id_pedido) on delete cascade;
--- alter table envio add constraint foreign key (id_restaurant) references restaurant(id_restaurant) on delete cascade;
-ALTER TABLE envio ADD CONSTRAINT FOREIGN KEY (id_cliente) REFERENCES cliente(id_cliente) on delete cascade;
-ALTER TABLE envio ADD CONSTRAINT FOREIGN KEY (id_pedido) REFERENCES pedido(id_pedido) on delete cascade;
 ALTER TABLE envio ADD CONSTRAINT FOREIGN KEY (id_restaurant) REFERENCES restaurant(id_restaurant) on delete cascade;
+ALTER TABLE restaurant ADD CONSTRAINT FOREIGN KEY (id_envio) REFERENCES envio(id_envio) on delete cascade;
+-- drop database FoodDash;
