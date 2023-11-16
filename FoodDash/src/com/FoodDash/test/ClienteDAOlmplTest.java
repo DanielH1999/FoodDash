57e9daa51fab32a5f059f7af31ea82385f712352
MarcosCobo03 <148403526+MarcosCobo03@users.noreply.github.com>
2023-11-15 21:26:51 -0300

    Update ClienteDAOlmplTest.java

--- FoodDash/src/com/FoodDash/test/ClienteDAOlmplTest.java
+++ FoodDash/src/com/FoodDash/test/ClienteDAOlmplTest.java
@@ -26,7 +26,7 @@ public class ClienteDAOlmplTest {
 
          ClienteDAOlmpl clienteDAOlmpl = new ClienteDAOlmpl();
          
-         clienteDAOlmpl.realizarPedido(cliente,pedido, restaurante);
+         clienteDAOlmpl.realizarPedido(cliente,pedido, 1);
          
          System.out.println(pedido.toString());
     }
