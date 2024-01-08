0ad4bb987b53d79e54f185123bfb0b673376cddd
Arroyo Herrero Daniel Gaspar <86022708+DanielH1999@users.noreply.github.com>
2024-01-08 01:47:29 -0300

    Add files via upload

---
+++ build/classes/com/FoodDash/sql/project_base.sql
@@ -0,0 +1,48 @@
+create database if not exists FoodDash;
+use FoodDash;
+create table if not exists cliente(
+id_cliente int not null,
+primary key (id_cliente),
+nombre varchar(20) not null,
+direccion varchar(40) not null,
+telefono varchar(10) not null
+);
+
+create table if not exists restaurant(
+id_restaurant int auto_increment not null,
+primary key (id_restaurant),
+id_envio int,
+nombre varchar(20) not null,
+telefono int
+);
+
+create table if not exists pedido(
+id_pedido int not null,
+id_cliente int not null,
+foreign key (id_cliente) references cliente(id_cliente) on delete cascade,
+id_restaurant int not null,
+foreign key (id_restaurant) references restaurant(id_restaurant) on delete cascade,
+primary key (id_cliente, id_restaurant),
+productos varchar(255),
+cantidades varchar(255),
+suma_precio float not null,
+tiempo_preparacion int,
+estado varchar(20)
+);
+
+create table if not exists envio(
+id_envio int auto_increment primary key,
+id_restaurant int not null,
+direccion varchar(40) not null,
+medio_pago varchar(10) not null,
+precio_envio float not null,
+estado varchar(20)
+);
+
+ALTER TABLE envio ADD CONSTRAINT FOREIGN KEY (id_restaurant) REFERENCES restaurant(id_restaurant) on delete cascade;
+ALTER TABLE restaurant ADD CONSTRAINT FOREIGN KEY (id_envio) REFERENCES envio(id_envio) on delete cascade;
+
+-- drop
+drop database FoodDash;
+
+-- que pasa cuando borramos un registro de cliente xor restaurant?
